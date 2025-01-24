2eea8ddb27456f99378631b14b5469a0df4c644c
Michail Tiurin <tyuris@icloud.com>
2025-01-24 12:33:01 +0300

    web app with backend

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,10 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-tasks = ["foo", "bar", "baz"]
+from django import forms
+from django.urls import reverse
+
+#tasks = [] if u wanna use without sessions
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    #priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = [] #for each sesion u can delete and activate upper one
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        #"tasks": tasks 
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
